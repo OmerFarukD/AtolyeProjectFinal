3027e1d6a8b1ac91d790cef655284620e2bc1e80
OmerFarukD <89133026+OmerFarukD@users.noreply.github.com>
2025-12-05 20:49:13 +0300

    Fake Data Added

--- FinalProject.Console/Program.cs
+++ FinalProject.Console/Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.WriteLine("1. gün verileri oluşturuldu.");
+
+string[] brands = {"BMW", "Mercedes","Audi"};
+string[] models = {"m5","Benz", "A3" };
+double[] dailyPrice = {2500,3658,4500};
+
+for (int i = 0; i <brands.Length; i++)
+{
+    Console.WriteLine($"Marka Adı : {brands[i]}, Model Adı : {models[i]}, Günlük  Fiyatı : {dailyPrice[i]}  TL");
+}
